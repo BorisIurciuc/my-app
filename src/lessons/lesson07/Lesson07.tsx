850b8a1e870fae3c5c0850cf263bb974bd7be5e6
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-06-20 17:50:33 +0200

    first commit

---
+++ src/lessons/lesson07/Lesson07.tsx
@@ -0,0 +1,22 @@
+export default function Leson07() {
+
+    
+
+    //1.Типизауия строк
+    let user: string = 'Donald'
+    user = 'Angela'
+
+    //2.Типизауия чисел
+
+    let importNum: number = 42
+    importNum = -10
+
+    return (
+        <>
+            <h1>React Typecsript</h1>
+            <p>{user}</p>
+            <p>{importNum}</p>
+        </>
+
+    )
+}
