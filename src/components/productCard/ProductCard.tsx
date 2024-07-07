9e23cf3748a88b3ffd183e1b031f5b9da860d8e7
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-07-07 12:13:29 +0200

    update ProductCard finally

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,13 +1,14 @@
+import { useState } from 'react';
 import styles from './productCard.module.css'
 
 interface IProductData {
     cardImage: string;
-    availability: true;
+    availability: boolean;
     thereIs: string;
     thereNot: string;
 }
 
-const productData: IProductData = {
+const initialProductData: IProductData = {
     cardImage:'https://cdn.motor1.com/images/mgl/OkpwL/s1/ferrari-296-gtb.jpg',
     availability: true,
     thereIs: "Спешите, на складе несколько экземпляров",
@@ -16,21 +17,24 @@ const productData: IProductData = {
 
 function ProductCard(){
 
-    
+    const [productData, setProductDara] = useState<IProductData>(initialProductData)
+
+    const handleAvalaibilityToggle = () => {
+        setProductDara(prevState => ({...prevState, availability: !prevState.availability}))
+    }
 
     return (
         <div className={styles.containerProduct}>
-            <h1>Наши предложения</h1>
-            <h4>Ferrari 296 GTB</h4>
-            <>
-                <img src={productData.cardImage} alt="ferrari" 
-                className={productData.availability ? styles.imgProductYes :  styles.imgProductNo}/>
-            </>
+            <h1>Ferrari 296 GTB</h1>
+            <img 
+                src={productData.cardImage} 
+                alt="ferrari" 
+                className={productData.availability ? styles.imgProductYes : styles.imgProductNo}
+            />
             <h4>Откройте новую эру скорости с Ferrari 296 GTB</h4>
             <p>
                 Позвольте себе ощутить неподдельный восторг от вождения с Ferrari 296 GTB — автомобилем, 
                 который воплощает в себе сочетание роскоши, инноваций и невероятной динамики.
-                <br />Инновации и мощь
                 <br />Ferrari 296 GTB оснащен революционным V6-двигателем с турбонаддувом и гибридной системой, 
                 которые вместе вырабатывают ошеломляющие 830 лошадиных сил. Благодаря этому автомобиль 
                 разгоняется до 100 км/ч всего за 2,9 секунды, обеспечивая максимальное удовольствие от вождения.
@@ -42,6 +46,13 @@ function ProductCard(){
                     {productData.availability ? productData.thereIs : productData.thereNot}
                     </p>
                 </b>
+                    <label htmlFor="availabilityCheckbox">изменить состояние</label>
+                    <input 
+                        type="checkbox" 
+                        id='availabilityCheckbox'
+                        checked={productData.availability}
+                        onChange={handleAvalaibilityToggle}
+                    />
             </div>
         </div>
     )
